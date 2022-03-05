eaa9c602a2007d80600a113ca851db112fbcb6d0
Tom Vempala <54186859+tomthomasvempala@users.noreply.github.com>
2022-03-06 04:18:06 +0530

    enthokeyo

---
+++ lib/Pages/Market/item.dart
@@ -0,0 +1,39 @@
+var types =["Electronics","Bags","Sports","Accesories"];
+
+var marketItem =[
+  {
+    "name":"Logitech Mouse",
+    "type":"Electronics",
+    "price":20,
+    "img":"url",
+    "desc":"Lore_Ipsum"
+  },
+  {
+    "name":"Adidas Backpack",
+    "type":"Bags",
+    "price":25,
+    "img":"url",
+    "desc":"Lore_Ipsum"
+  },
+  {
+    "name":"Nike Soccer Boots",
+    "type":"Sports",
+    "price":25,
+    "img":"url",
+    "desc":"Lore_Ipsum"
+  },
+  {
+    "name":"Puma Soccer Boots",
+    "type":"Sports",
+    "price":23,
+    "img":"url",
+    "desc":"Lore_Ipsum"
+  },
+  {
+    "name":"Rolex Watch",
+    "type":"Accesories",
+    "price":40,
+    "img":"url",
+    "desc":"Lore_Ipsum"
+  },
+];
