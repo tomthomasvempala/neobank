f6ee33d0faf2a9c9429dbaf6cf9926b133e129ea
Tom Vempala <54186859+tomthomasvempala@users.noreply.github.com>
2022-03-06 11:03:29 +0530

    not for devcraft

--- lib/Pages/Market/market.dart
+++ lib/Pages/Market/market.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:neobank/Pages/Market/item.dart';
 
 class Market extends StatefulWidget {
   // const Market({ Key? key }) : super(key: key);
@@ -10,12 +11,18 @@ class Market extends StatefulWidget {
 class _MarketState extends State<Market> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-      ), itemBuilder: (context, index){
-        return Container(); 
-      }),
+    return Scaffold(
+      appBar: AppBar(title: Text("Market"),),
+          body: Container(
+        child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          
+        ), 
+        itemCount: marketItem.length,
+        itemBuilder: (context, index){
+          return Container(height: 20,width: 20,color: Colors.red,); 
+        }),
+      ),
     );
   }
 }
